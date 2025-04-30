03b92e907bce146db05c2447b016250bc690ad42
ryohei1216 <ryohei.miyamoto@gaudiy.com>
2025-04-30 22:26:30 +0900

    chore: Add basic authentication

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['api.qrserver.com'],
   },
+  // Expose environment variables to the Edge runtime
+  env: {
+    BASIC_AUTH_USERNAME: process.env.BASIC_AUTH_USERNAME,
+    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
+  },
 };
 
 export default nextConfig;
